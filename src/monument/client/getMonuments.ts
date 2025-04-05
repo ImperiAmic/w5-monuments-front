e75041871db4a2e3383ab398a928b0e936de5fd2
Raimon <raimon.txollo@gmail.com>
2025-04-05 17:00:33 +0200

    Move getMonument fetch function to client module within monument entity

---
+++ src/monument/client/getMonuments.ts
@@ -0,0 +1,11 @@
+import { Monument } from "../types";
+
+const getMonuments = async (url: string): Promise<Monument[]> => {
+  const response = await fetch(url);
+
+  const { monuments } = (await response.json()) as { monuments: Monument[] };
+
+  return monuments;
+};
+
+export default getMonuments;
